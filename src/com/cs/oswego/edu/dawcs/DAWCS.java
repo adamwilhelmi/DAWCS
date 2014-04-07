eb9bb44dc35b785e1de5739a16fd053a850dbc4a
Adam Wilhelmi <awilhelm@oswego.edu>
2014-04-07 16:04:35 -0400

    Adam - fixing repo/merging

--- src/com/cs/oswego/edu/dawcs/DAWCS.java
+++ src/com/cs/oswego/edu/dawcs/DAWCS.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 package com.cs.oswego.edu.dawcs;
 
 import java.util.ArrayList;
@@ -66,106 +64,7 @@ public class DAWCS extends Activity {
     }
     
     public void addChannel(final LinearLayout ll){
-        final ChannelController newChannel = new ChannelController(this, true, true, true);
-        newChannel.addCloseListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                removeChannel(newChannel, ll);
-            }
-        });
-        
-//        newChannel.requestDisallowInterceptTouchEvent(true);
-        
-        channels.add(newChannel);
-        ll.addView(newChannel);
-        genBufferView(newChannel);
-        ll.addView(bufferMap.get(newChannel));
-        
-        
-    }
-    public void removeChannel(ChannelController cc, LinearLayout ll){
-        channels.remove(cc);
-        ll.removeView(cc);
-        ll.removeView(bufferMap.get(cc));
-        bufferMap.remove(cc);
-    }
-    
-    private void genBufferView(ChannelController cc){
-    	TextView t = new TextView(getBaseContext());
-    	t.setText("  ");
-    	
-    	bufferMap.put(cc, t);
-    }
-}
-    
-=======
->>>>>>> 409d3fbcaf4d9af73bcb85b914da484205a1638e
-package com.cs.oswego.edu.dawcs;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import android.net.wifi.WifiManager;
-import android.os.Bundle;
-import android.os.StrictMode;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-public class DAWCS extends Activity {
-    final ArrayList<ChannelController> channels = new ArrayList<ChannelController>();
-
-    public static final int NUM_STREAMS = 1;
-    
-    HashMap<ChannelController, View> bufferMap = new HashMap<ChannelController, View>();
-    ArrayList<Integer> unavailChannels = new ArrayList<Integer>();
-    
-    Button addChannel;
-    Button toggleScroll;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_dawcs);
-        final LinearLayout ll = (LinearLayout)findViewById(R.id.chanHolder);
-        ll.setBackgroundColor(0xffa9a9a9);
-        
-        NonFocusingHorizontalScrollView hsv = (NonFocusingHorizontalScrollView) findViewById(R.id.scroll1);
-        
-        for (int i = 0; i < 10; i++) { //for debugging/testing only - adds 10 channels on load
-			addChannel(ll);
-		}
-		addChannel = (Button) findViewById(R.id.add_channel);
-        addChannel.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                addChannel(ll);
-            }
-        });
-
-        NetHandler.getInstance().setWifiManager(((WifiManager) this.getSystemService(DAWCS.WIFI_SERVICE)));
-       
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        NetHandler.getInstance().invokeListenerThread();
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.dawc, menu);
-        
-        return true;
-    }
-    
-    public void addChannel(final LinearLayout ll){
-        final ChannelController newChannel = new ChannelController(this, true, true, true);
+        final ChannelController newChannel = new ChannelController(this, true, true, true, new Channel(1, false));
         newChannel.addCloseListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
